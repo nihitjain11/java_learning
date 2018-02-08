b046aa1b9e713a3d2c10c0b6f016ba3afb2f2a26
Nihit Jain <nihitjain11@users.noreply.github.com>
2018-02-08 12:59:18 +0530

    Update arrayTable.java

--- arrayTable.java
+++ arrayTable.java
@@ -1,31 +1,38 @@
 class Array
 {
-  int x[] = new int[5];
-  Array(int a,int b,int c,int d,int e) {
+  int x[] = new int[10];
+  Array(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j) {
     x[0] = a;
     x[1] = b;
     x[2] = c;
     x[3] = d;
     x[4] = e;
+    x[5] = f;
+    x[6] = g;
+    x[7] = h;
+    x[8] = i;
+    x[9] = j;
   }
-  void traverse(int k) {
+  void table(int k) {
     double start =  System.currentTimeMillis();
     for (int i=1;i<11;i++) {
       System.out.println(k+" * "+i+" = "+(k*i));
     }
     double end = System.currentTimeMillis();
-    System.out.println("\nTime taken by table print = "+(end-start)+" ms\n");
+    System.out.println("\nTime taken in printing above Table = "+(end-start)+" ms\n");
   }
 }
 
 class Main
 {
   public static void main(String args[]) {
-    Array ob1 = new Array(9,8,7,6,5);
-    Array ob2 = new Array(1,2,3,4,5);
-    System.out.print("\nTraversing... ");
-    for (int i=0;i<5;i++){
-      ob1.traverse(ob1.x[i]);
+    double start =  System.currentTimeMillis();
+    Array ob1 = new Array(1,2,3,4,5,6,7,8,9,10);
+    System.out.println("\nTables... \n");
+    for (int i=0;i<10;i++){
+      ob1.table(ob1.x[i]);
     }
+    double end = System.currentTimeMillis();
+    System.out.println("\nCumulative time of Program Execution = "+(end-start)+" ms\n");
   }
 }
